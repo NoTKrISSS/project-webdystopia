f658ce98cda27e9cb7b97a46916988eaae80b917
NoTKrISSS <mainkshyska@gmail.com>
2021-07-31 19:09:00 +0300

    initial commit testing js

---
+++ README.md
@@ -0,0 +1,21 @@
+# webdystopia
+
+_personal project for fun_
+
+This project is for educational porpuses only.
+
+Site published at 
+
+DESCRIPTION: A chrome extention that tries to change the web experience into a dystopian version of reality xD
+
+HOW TO USE:
+
+## Project features
+
+- Github pages
+- Chrome extention
+- Manifest file
+
+## Authors
+
+[NoTKrISSS](https://github.com/NoTKrISSS)
---
+++ app.js
@@ -0,0 +1,14 @@
+let petras = [
+    "https://media.lrytas.lt/images/2020/12/08/082054419-42c95415-7cbf-4b3d-ba33-fd5ee2be41c4.png",
+    "https://lithuaniatribune.com/wp-content/uploads/2019/04/4852931.jpg",
+    "https://www.lgl.lt/en/files/grazulis.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZwImkg-RBgFlTIR5JtJSePm0bon2zczwbYQ&usqp=CAU",
+    "https://lithuaniatribune.com/wp-content/uploads/2019/04/7448714.jpg"
+];
+
+
+const imgs = document.getElementsByTagName("img")
+for (let i = 0; i < imgs.length; i++){
+    const randomImg = Math.floor(Math.random() * petras.length);
+    img[i].src = petras[randomImg];
+}
